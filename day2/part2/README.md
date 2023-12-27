45ffcb2edfc78a6faa5cde6f5efe2500f2adf892
liolle <44002141+liolle@users.noreply.github.com>
2023-12-27 16:44:19 +0100

    [Add] day2 part2

---
+++ day2/part2/README.md
@@ -0,0 +1,13 @@
+## Cube Conundrum
+
+[Challenge](https://adventofcode.com/2023/day/2)
+
+## Solution
+
+To solve part 2 we want for each game to keep track of the maximum for each color.
+
+## Demo
+
+```bash
+bash two.sh <path-to-input>
+```
---
+++ day2/part2/two.sh
@@ -0,0 +1,59 @@
+#!/bin/bash"
+
+declare -A rules
+
+rules[red]=0
+rules[green]=0
+rules[blue]=0
+
+extract_id () {
+	if [[ $1 =~ [0-9]+ ]]; then
+        return ${BASH_REMATCH[0]}
+	fi
+	return 20
+}
+
+read_file(){
+
+	if [[ -z $1 ]]; then
+        echo "Need an input"
+        return 1
+    fi
+
+    total=0
+
+	
+	while read -r line; do
+		IFS=':' read -ra elements <<< "$line"
+		IFS=';' read -ra rounds <<< "${elements[1]}"
+		extract_id "${elements[0]}"
+		game_id=$?
+
+
+		for (( i = 0; i < ${#rounds[@]}; i++ )); do
+			IFS=',' read -ra draws <<< "${rounds[$i]}"
+			for (( j = 0; j < ${#draws[@]}; j++ )); do
+				IFS=' ' read -ra draw <<< "${draws[$j]}"
+				number=${draw[0]}
+				color=${draw[1]}
+				if [[ $number -gt ${rules[$color]} ]]; then
+					rules["$color"]=$number
+					
+				fi
+			done
+			
+		done
+
+		total=$((${rules[red]}*${rules[green]}*${rules[blue]} + $total))
+
+		rules[red]=0
+		rules[green]=0
+		rules[blue]=0
+
+    done < $1
+
+    echo $total
+
+}
+
+read_file $1
